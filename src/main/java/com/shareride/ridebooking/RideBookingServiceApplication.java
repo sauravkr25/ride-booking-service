113085ca918efa3e1f5554c7ddffc7cf74a7c0bb
sauravkr25 <90563802+sauravkr25@users.noreply.github.com>
2025-08-29 01:44:50 +0530

    first commit

---
+++ src/main/java/com/shareride/ridebooking/RideBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shareride.ridebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBookingServiceApplication.class, args);
+	}
+
+}
